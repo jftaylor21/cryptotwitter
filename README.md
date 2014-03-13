448135ff6e317f7e3e1b2d1b2e789e9b229891fb
jftaylor21 <jftaylor21@gmail.com>
2014-03-12 22:54:28 -0400

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-cryptotwitter
-=============
+This is dependent on:
+1. https://github.com/tweepy/tweepy
---
+++ cryptotwitter.py
@@ -0,0 +1,10 @@
+import config
+
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+auth.set_access_token(config.ACCESS_KEY, config.ACCESS_SECRET)
+api = tweepy.API(auth)
+
+api.update_status('test')
