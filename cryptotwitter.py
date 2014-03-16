fad6b69c6983c9ba7060676c8d4bba958c92e1f9
jftaylor21 <jftaylor21@gmail.com>
2014-03-16 02:32:36 -0400

    Refactored history and changed file format. You will need to delete the old data/history.pkl

--- cryptotwitter.py
+++ cryptotwitter.py
@@ -7,8 +7,9 @@ import json
 import urllib
 import cPickle as pickle
 import sys
+import collections
 
-class History:
+class HistoryItem:
   def __init__(self, cointype, coindata, ticker, usdPerBtc):
     # minimum data needed to calculate other data
     self.cointype = cointype
@@ -18,14 +19,74 @@ class History:
     self.btcPerCoin = float(ticker[self.tickerkey])
     self.usdPerBtc = usdPerBtc
     
-    # calculated data
-    self.btc = self.balance * self.btcPerCoin
-    self.usd = self.btc * self.usdPerBtc
-    
+  @property
+  def btc(self):
+    return self.balance * self.btcPerCoin
+  
+  @property
+  def usd(self):
+    return self.btc * self.usdPerBtc
+  
   def __str__(self):
     return getTimeString()+': '+self.cointype+': '+self.address+' balance: '+repr(self.balance)+' '+self.tickerkey+': '+repr(self.btcPerCoin)+' bitcoin: '+coin2str(self.btc)+ ' $'+usd2str(self.usd)
 
+class History:
+  '''Keeps numEntries of history for each coin type'''
+  def __init__(self, numEntries, filename=None):
+    self.numEntries = numEntries
+    self.load(filename)
+    
+  def addHistory(self, item):
+    # Can only add one item at a time.
+    # maxlen of the deque will remove old entries automatically
+    self._history.setdefault(item.cointype, collections.deque(maxlen=self.numEntries))
+    self._history[item.cointype].append(item)
+    
+  def load(self, filename):
+    # load will clear current history and load history from file.
+    # no errors will occur if opening the file fails. This is assumed to be the
+    # case where it is the first time the program is run and there is no
+    # history.
+    self._history = {}
+    if filename is not None:
+      try:
+        hfile = open(filename, 'rb')
+        self._history = pickle.load(hfile)
+        hfile.close()
+      except IOError:
+        pass
+      
+  def save(self, filename):
+    hfile = open(filename, 'wb')
+    pickle.dump(self._history, hfile)
+    hfile.close()
+  
+  def totalInBTC(self, entry):
+    # entry 0 is most current
+    total = 0
+    for cointype in self._history:
+      historylen = len(self._history[cointype])
+      index = historylen-entry-1
+      if index >= 0 and index < historylen:
+        total = total + self._history[cointype][index].btc
+      else: # should there be an error here?
+        print('Index '+str(index)+' out of valid ranges of 0-'+str(historylen))
+    return total
+    
+  def totalInUSD(self, entry):
+    # entry 0 is most current
+    total = 0
+    for cointype in self._history:
+      historylen = len(self._history[cointype])
+      index = historylen-entry-1
+      if index >= 0 and index < historylen:
+        total = total + self._history[cointype][index].usd
+      else: # should there be an error here?
+        print('Index '+str(index)+' out of valid ranges of 0-'+str(historylen))
+    return total
+
 def readurl(url):
+  # need to add check for errors
   urlh = urllib.urlopen(url)
   text = urlh.read()
   urlh.close()
@@ -93,39 +154,20 @@ def writeOutputAndSleep(twitter, output, secondsToNextUpdate):
     printNextUpdate(secondsToNextUpdate)
   time.sleep(secondsToNextUpdate)
   
-def loadHistory():
-  try:
-    hfile = open(config.HISTORY, 'rb')
-    history = pickle.load(hfile)
-    hfile.close()
-    return history
-  except IOError:
-    return []
-  
-def saveHistory(history):
-  hfile = open(config.HISTORY, 'wb')
-  pickle.dump(history, hfile)
-  hfile.close()
-  
 def getSummaryOutput(history, prevhistory):
+  totalBtc = history.totalInBTC(0)
+  pTotalBtc = prevhistory.totalInBTC(0)
+  
+  totalUsd = history.totalInUSD(0)
+  pTotalUsd = history.totalInUSD(0)
+  
+  # average value per bitcoin during this time
   usdPerBtc = 0
+  if totalBtc > 0:
+    usdPerBtc = totalUsd / totalBtc
   pUsdPerBtc = 0
-  
-  totalBtc = 0
-  pTotalBtc = 0
-  
-  totalUsd = 0
-  pTotalUsd = 0
-  
-  for h in history:
-    usdPerBtc = h.usdPerBtc
-    totalBtc = totalBtc + h.btc
-    totalUsd = totalUsd + h.usd
-    
-  for h in prevhistory:
-    pUsdPerBtc = h.usdPerBtc
-    pTotalBtc = pTotalBtc + h.btc
-    pTotalUsd = pTotalUsd + h.usd
+  if pTotalBtc > 0:
+    pUsdPerBtc = pTotalUsd / pTotalBtc
     
   return getTimeString()+': $/Bitcoin: '+delta2str(usd2str(usdPerBtc), usd2str(pUsdPerBtc))+' total BTC: '+delta2str(coin2str(totalBtc), coin2str(pTotalBtc))+' total: $'+delta2str(usd2str(totalUsd), usd2str(pTotalUsd))
   
@@ -141,12 +183,12 @@ if __name__ == "__main__":
   while(True):
     ticker = getPoloniexTicker()
     usdPerBtc = getUSDPerBitcoin()
-    prevhlist = loadHistory()
-    hlist = []
+    prevhistory = History(1, config.HISTORY)
+    history = History(1)
     for cointype in wallet:
-      h = History(cointype, wallet[cointype], ticker, usdPerBtc)
-      hlist.append(h)
+      h = HistoryItem(cointype, wallet[cointype], ticker, usdPerBtc)
+      history.addHistory(h)
       writeOutputAndSleep(twitter, str(h), config.SLEEP_COIN)
-    output = getSummaryOutput(hlist, prevhlist)
-    saveHistory(hlist)
+    output = getSummaryOutput(history, prevhistory)
+    history.save(config.HISTORY)
     writeOutputAndSleep(twitter, output, config.SLEEP_LOOP)
